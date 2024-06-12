69115aef11da041574eea7e31a54459a0785ce32
xihi <xuhui0607@gmail.com>
2024-06-12 14:05:56 +0800

    添加XHTV更新文件

---
+++ xhtv/dev.bat
@@ -0,0 +1,16 @@
+move .\leanbackJavaArm64_v8a\release\*.apk .\dev
+move .\leanbackJavaArmeabi_v7a\release\*.apk .\dev
+move .\leanbackPythonArm64_v8a\release\*.apk .\dev
+move .\leanbackPythonArmeabi_v7a\release\*.apk .\dev
+move .\mobileJavaArm64_v8a\release\*.apk .\dev
+move .\mobileJavaArmeabi_v7a\release\*.apk .\dev
+move .\mobilePythonArm64_v8a\release\*.apk .\dev
+move .\mobilePythonArmeabi_v7a\release\*.apk .\dev
+rmdir /s /q .\leanbackJavaArm64_v8a
+rmdir /s /q .\leanbackJavaArmeabi_v7a
+rmdir /s /q .\leanbackPythonArm64_v8a
+rmdir /s /q .\leanbackPythonArmeabi_v7a
+rmdir /s /q .\mobileJavaArm64_v8a
+rmdir /s /q .\mobileJavaArmeabi_v7a
+rmdir /s /q .\mobilePythonArm64_v8a
+rmdir /s /q .\mobilePythonArmeabi_v7a
